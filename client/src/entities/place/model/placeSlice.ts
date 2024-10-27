c4fb9bec9cb5b5161ec4f037b6740b13625cd81b
Dmitry <dmi.pol@icloud.com>
2024-10-27 17:04:12 +0300

    bug fixed

--- client/src/entities/place/model/placeSlice.ts
+++ client/src/entities/place/model/placeSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { Place, PlaceList } from ".";
-// import { refreshAccessToken, signIn, signUp, logout } from '../api/userThunks';
 import { message } from "antd";
 import {
   addPlace,
@@ -38,22 +37,20 @@ const placeSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-
-    .addCase(getOnePlace.pending, (state) => {
-      state.loading = true;
-    })
-    .addCase(getOnePlace.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload?.message || "Error to load";
-      message.error(state.error);
-    })
-    .addCase(getOnePlace.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.place = action.payload; 
-    })
-    // -----------------------
-
+      .addCase(getOnePlace.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getOnePlace.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Error to load";
+        message.error(state.error);
+      })
+      .addCase(getOnePlace.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.place = action.payload; 
+      })
+      // -----------------------
       .addCase(getApprovedPlaces.pending, (state) => {
         state.loading = true;
       })
@@ -65,28 +62,23 @@ const placeSlice = createSlice({
       .addCase(getApprovedPlaces.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
-        state.approvedPlaces = action.payload
-        console.log(action.payload, 'APPPROVED');
+        state.approvedPlaces = action.payload;
       })
-      //!-----------------------
-=
-    
-         .addCase(getPendingPlaces.pending, (state) => {
-          state.loading = true;
-        })
-        .addCase(getPendingPlaces.rejected, (state, action) => {
-          state.loading = false;
-          state.error = action.payload?.message || "Error to load";
-          message.error(state.error);
-        })
-        .addCase(getPendingPlaces.fulfilled, (state, action) => {
-          state.loading = false;
-          state.error = null;
-          state.pendingPlaces = action.payload
-          console.log(action.payload, 'PENDDDDDDING');
-        })
-             // -----------------------
-=
+      // -----------------------
+      .addCase(getPendingPlaces.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getPendingPlaces.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Error to load";
+        message.error(state.error);
+      })
+      .addCase(getPendingPlaces.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.pendingPlaces = action.payload;
+      })
+      // -----------------------
       .addCase(addPlace.pending, (state) => {
         state.loading = true;
       })
@@ -98,12 +90,9 @@ const placeSlice = createSlice({
       .addCase(addPlace.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
-        console.log(action.payload);
         state.places.push(action.payload);
       })
       // -----------------------
-    
-      //!-----------------------
       .addCase(approvePlace.pending, (state) => {
         state.loading = true;
       })
@@ -119,7 +108,7 @@ const placeSlice = createSlice({
           place.id === action.payload.id ? action.payload : place
         );
       })
-      //!-----------------------
+      // -----------------------
       .addCase(rejectPlace.pending, (state) => {
         state.loading = true;
       })
@@ -131,10 +120,9 @@ const placeSlice = createSlice({
       .addCase(rejectPlace.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
-        state.places = state.places.filter(place => place.id !== action.payload.id);  
-        
+        state.places = state.places.filter(place => place.id !== action.payload.id);
       })
-      //!-----------------------
+      // -----------------------
       .addCase(removePlace.pending, (state) => {
         state.loading = true;
       })
@@ -150,7 +138,7 @@ const placeSlice = createSlice({
           (place) => place.id !== action.payload
         );
       })
-      //!-----------------------
+      // -----------------------
       .addCase(updatePlace.pending, (state) => {
         state.loading = true;
       })
@@ -178,28 +166,26 @@ const placeSlice = createSlice({
       .addCase(addPhoto.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
-        console.log(action.payload);
         state.places = state.places.map((place) =>
           place.id === action.payload.id ? action.payload : place
         );
       })
-      //! -----------------------
+      // -----------------------
       .addCase(removePhoto.pending, (state) => {
         state.loading = true;
       })
       .addCase(removePhoto.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload?.message || "Error to add";
+        state.error = action.payload?.message || "Error to remove";
         message.error(state.error);
       })
       .addCase(removePhoto.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
-        console.log(action.payload);
         state.places = state.places.map((place) =>
           place.id === action.payload.id ? action.payload : place
         );
-      })
+      });
   },
 });
 
