17ab16677541c35b2c6d8ccaa4151a15fd10d34c
MaryV10 <mary10@list.ru>
2024-10-28 19:50:53 +0300

    fixed

--- client/src/entities/place/ui/MyPlaceItem/MyPlaceItem.tsx
+++ client/src/entities/place/ui/MyPlaceItem/MyPlaceItem.tsx
@@ -3,56 +3,19 @@ import styles from "./MyPlaceItem.module.css";
 import { Steps } from "antd"; 
 
 import { LoadingOutlined,CloseOutlined} from '@ant-design/icons';
-import { useAppDispatch } from "@/shared/hooks/reduxHooks";
-
-import { removePhoto } from "@/entities/photo/api/photoThunks";
 
 import { Place } from "../../model";
 import { Link } from "react-router-dom";
 
 
-// import { Place, PlaceWithoutStatusTagsPhotosFeedbacks } from "../../model";
-
 type Props = {
   place: Place;
-  // onPlaceDelete: (id: number) => void;
-  // onPlaceUpdate: (updatedPlace: PlaceWithoutStatusTagsPhotosFeedbacks) => void;
 };
 
 export const MyPlaceItem: React.FC<Props> = ({
   place,
-  // onPlaceDelete,
-  // onPlaceUpdate,
+
 }) => {
-  const dispatch = useAppDispatch();
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [title, setTitle] = useState(place.title);
-  // const [description, setDescription] = useState(place.description);
-  // const [width, setWidth] = useState(place.width);
-  // const [longitude, setLongitude] = useState(place.longitude);
-
-  // const handleEdit = () => {
-  //   setIsEditing(true);
-  // };
-
-  // const onDeleteHandeler = () => {
-  //   dispatch(removePhoto({ id: place.Photos[0].id}));
-  // };
-
-  // const handleCancel = () => {
-  //   setIsEditing(false);
-  //   setTitle(place.title);
-  //   setDescription(place.description);
-  // };
-
-  // const handleSave = async () => {
-  //   try {
-  //     onPlaceUpdate({ id: place.id, title, description, width, longitude, userId: place.userId });
-  //     setIsEditing(false);
-  //   } catch (error) {
-  //     console.error("Error updating place:", error);
-  //   }
-  // };
 
   const steps = [
     {
@@ -83,46 +46,9 @@ export const MyPlaceItem: React.FC<Props> = ({
           
     <img style={{height: '20vh', margin: '10px', borderRadius: '10px'}} src={place.Photos[0].imageUrl} alt='1' />  
 
-          {/* <button onClick={handleEdit}>Edit</button>
-          <button onClick={() => onPlaceDelete(place.id)}>Delete</button> */}
       </>
       </div>
-    // </div>
+
   );
 }
 
-
-{/* {isEditing ? (
-        <>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title"
-            className={styles.input}
-          />
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Task Description"
-            className={styles.input}
-          />
-          <input
-            type="text"
-            value={width}
-            onChange={(e) => setWidth(e.target.value)}
-            placeholder="Width"
-            className={styles.input}
-          />
-          <input
-            type="text"
-            value={longitude}
-            onChange={(e) => setLongitude(e.target.value)}
-            placeholder="Longitude"
-            className={styles.input}
-          />
-          <button onClick={handleSave}>Save</button>
-          <button onClick={handleCancel}>Cancel</button>
-        </>
-      ) : ( */}
