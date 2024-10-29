dfdec2d0608f03a61c1a2d7398b02887ebae502a
Dmitry <dmi.pol@icloud.com>
2024-10-29 18:56:46 +0300

    styles for one place page and some bug fixes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,14 +6,18 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 
 import { YMaps } from "@pbe/react-yandex-maps";
+import { Footer } from "@/widgets/Footer";
 
 function App() {
   return (
+    
     <YMaps>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
     </YMaps>
+   
+  
   );
 }
 
