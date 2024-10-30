2a6ed3d983e67b154047c6df6c10b2fef8b95847
Andrey Veselov <lifeforglory@gmail.com>
2024-10-30 13:00:31 +0300

    fix:form field fixed

--- client/src/widgets/ProfileForm/ProfileForm.tsx
+++ client/src/widgets/ProfileForm/ProfileForm.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ProfileForm.module.css";
 
-import { getUser, updateUser } from '@/entities/user/api/userThunks';
+import { getUser, updateUser } from "@/entities/user/api/userThunks";
 import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHooks";
-
 
 const ProfileForm: React.FC = () => {
   const [nickname, setNickname] = useState("");
@@ -11,26 +10,22 @@ const ProfileForm: React.FC = () => {
   const [secondName, setSecondName] = useState("");
   const [isEditing, setIsEditing] = useState(false);
 
-
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.user);
 
-  useEffect(() => {  
-    if (user?.id) {  
-      const fetchUser = async () => {  
-        try {  
-          await dispatch(getUser(user.id));  
-        } catch (error) {  
-          console.error('Error fetching user:', error);  
-        }  
-      };  
+  useEffect(() => {
+    if (user?.id) {
+      const fetchUser = async () => {
+        try {
+          await dispatch(getUser(user.id));
+        } catch (error) {
+          console.error("Error fetching user:", error);
+        }
+      };
 
-      fetchUser();  
-    }  
-  }, [dispatch, user?.id]); 
-
-  
-
+      fetchUser();
+    }
+  }, [dispatch, user?.id]);
 
   const handleEdit = () => {
     setIsEditing(true);
@@ -39,25 +34,20 @@ const ProfileForm: React.FC = () => {
       setFirstName(user.firstName);
       setSecondName(user.secondName);
     }
- 
   };
 
   const handleCancel = () => {
-    
     if (user) {
       setIsEditing(false);
-  
     }
   };
 
-  const handleSave = ( event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSave = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    
-    try {
-    
-     dispatch(updateUser({ nickname, firstName, secondName }))
 
-      
+    try {
+      dispatch(updateUser({ nickname, firstName, secondName }));
+
       setIsEditing(false);
     } catch (error) {
       console.error("Error updating task:", error);
@@ -65,59 +55,81 @@ const ProfileForm: React.FC = () => {
   };
 
   return (
-    <div
-      className={styles.container}
-    >
-       <div>
+    <div className={styles.container}>
+      <div>
         {" "}
         <img
-          style={{  height: "100px", width: "100px" , borderRadius: "50%",objectFit: "cover", marginBottom: "20px"}}
+          style={{
+            height: "100px",
+            width: "100px",
+            borderRadius: "50%",
+            objectFit: "cover",
+            marginBottom: "20px",
+          }}
           src={user?.avatarUrl}
           alt="avatar"
         />
-      <div style={{ textAlign: "left" , background: "#f1e8d9", padding: "10px", borderRadius: "10px"}}>
-        <p>Карточка Профиля</p>
-        <p>Никнейм: {user?.nickname}</p>
-        <p>Имя: {user?.firstName}</p>
-        <p> Фамилия: {user?.secondName}</p>
-        <p>Email: {user?.email}</p>
-        {/* <p>Профиль создан: {user?.createdAt}</p> */}
-      </div>
-     
+        <div
+          style={{
+            textAlign: "left",
+            background: "#f1e8d9",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+        >
+          <p>Карточка Профиля</p>
+          <p>Никнейм: {user?.nickname}</p>
+          <p>Имя: {user?.firstName}</p>
+          <p> Фамилия: {user?.secondName}</p>
+          <p>Email: {user?.email}</p>
+          {/* <p>Профиль создан: {user?.createdAt}</p> */}
+        </div>
       </div>
       {isEditing ? (
         <>
-       
           <input
             type="text"
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
             placeholder="Nickname"
-            style={{ backgroundColor:'white'}}
+            style={{ backgroundColor: "white" }}
           />
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             placeholder="First Name"
-            style={{ backgroundColor:'white'}}
+            style={{ backgroundColor: "white" }}
           />
           <input
             type="text"
             value={secondName}
             onChange={(e) => setSecondName(e.target.value)}
             placeholder="Second Name"
-            style={{ backgroundColor:'white'}}
+            style={{ backgroundColor: "white" }}
           />
-          <button style={{ backgroundColor:'white', padding: "10px" }}onClick={handleSave}>Save</button>
-          <button style={{ backgroundColor:'white', padding: "10px" }}onClick={handleCancel}>Cancel</button>
-         
+          <button
+            style={{ backgroundColor: "white", padding: "10px" }}
+            onClick={handleSave}
+          >
+            Save
+          </button>
+          <button
+            style={{ backgroundColor: "white", padding: "10px" }}
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
         </>
       ) : (
         <>
-          <button style={{ backgroundColor:'white', padding: "10px" }}onClick={handleEdit}>Edit</button>
-      
-          </>
+          <button
+            style={{ backgroundColor: "white", padding: "10px" }}
+            onClick={handleEdit}
+          >
+            Редактировать
+          </button>
+        </>
       )}
     </div>
   );
